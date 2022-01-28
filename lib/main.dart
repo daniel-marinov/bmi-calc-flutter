6b14f34005eb5badd6f028e9d5399d3e86198116
Daniel Marinov <daniellmarinoff@gmail.com>
2022-01-28 16:35:54 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:first_app/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: HomeScreen(),
+    );
+  }
+}
